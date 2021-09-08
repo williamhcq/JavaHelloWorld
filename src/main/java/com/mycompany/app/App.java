750a4580aeec95b53fdd76ecf1a5bd53aea0f0d1
williamhcq <P1314782@NCS-270820YI12>
2021-09-08 22:33:56 +0800

    test2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! test1";
+    private final String message = "Hello World! test2";
 
     public App() {}
 
